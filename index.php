a76e57e4adbffb8a68998ef94951f841a35e86db
Salvi Pascual <salvi.pascual@gmail.com>
2021-08-07 01:13:15 -0400

    infinite scroll

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 		$canonical = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'];
 		$title = "Súmate a #LdeLibertad";
 		$description = "Súmate a miles de cubanos que publican su #LdeLibertad porque quieren vivir en una Cuba libre. Sube tu foto y sé parte del movimiento.";
+
+		$pictures = glob("people/*.*");
+		shuffle($pictures);
+
+		$picturesInit = array_slice($pictures, 0, 20);
+		$picturesLoad = array_slice($pictures, 20);
 	?>
 
 	<!-- meta -->
@@ -33,7 +39,7 @@
 	<!-- Google Font -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Mrs+Sheppards&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Knewave&display=swap" rel="stylesheet">
 
 	<!-- Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-86XRDSV4CG"></script>
@@ -47,21 +53,17 @@
 <body>
 	<div class="container-fluid">
 		<!-- images -->
-		<div class="row row-cols-2">
-			<?php 
-				$pictures = glob("people/*.*");
-				shuffle($pictures);
-				foreach ($pictures as $filename) { 
-			?>
-				<img class="img-fluid col-sm p-0" src="<?= $filename ?>" alt="">
+		<div id="images" class="row row-cols-2">
+			<?php foreach ($picturesInit as $filename) { ?>
+				<img class="img-fluid col-sm col-lg-3 p-0" src="<?= $filename ?>" alt="">
 			<?php } ?>
 	
 			<!-- buttons -->
 			<div class="position-fixed text-center text-md-end btn-list">
 				<a class="btn btn-join btn-dark btn-lg me-1" target="_blank" href="mailto:LdeLibertad1@gmail.com?subject=Mi foto de libertad&body=Adjunta un selfie con la L de libertad, y (si lo deseas) pon tu nombre, dónde vives, y un texto corto diciendo por qué quieres libertad"><i class="fas fa-plus-square"></i> Súmate</a>
-				<a class="btn btn-join btn-dark btn-lg me-1" target="_blank"	href="https://www.instagram.com/ldelibertad1_/"><i class="fab fa-instagram-square"></i></a>
-				<a class="btn btn-join btn-dark btn-lg me-1" target="_blank"	href="https://www.facebook.com/profile.php?id=100068017934767"><i class="fab fa-facebook-square"></i></a>
-				<a class="btn btn-join btn-dark btn-lg" target="_blank"	href="https://twitter.com/lde_libertad1/"><i class="fab fa-twitter-square"></i></a>
+				<a class="btn btn-join btn-dark btn-lg me-1" target="_blank" href="https://www.instagram.com/ldelibertad1_/"><i class="fab fa-instagram-square"></i></a>
+				<a class="btn btn-join btn-dark btn-lg me-1" target="_blank" href="https://www.facebook.com/profile.php?id=100068017934767"><i class="fab fa-facebook-square"></i></a>
+				<a class="btn btn-join btn-dark btn-lg" target="_blank" href="https://twitter.com/lde_libertad1/"><i class="fab fa-twitter-square"></i></a>
 			</div>
 		</div>
 
@@ -71,11 +73,11 @@
 
 	<style type="text/css">
 		.title {
-			font-family: 'Mrs Sheppards', cursive;
+			font-family: 'Knewave', cursive;
 			font-size: 13vw;
 			color: darkred;
 			text-shadow: 5px 5px 20px black;
-			-webkit-text-stroke: 3px white;
+			-webkit-text-stroke: 1px white;
 			margin-right: -50%;
 			transform: translate(-50%, -50%);
     	}
@@ -86,6 +88,7 @@
 		}
 
 		.btn-join {
+			font-family: sans-serif;
 			background-color: darkred;
 			border-radius: 25px;
 			border: 2px solid white;
@@ -94,5 +97,32 @@
 
 	<!-- font awesome -->
 	<script src="https://kit.fontawesome.com/17fb4c8456.js" crossorigin="anonymous"></script>
+
+	<!-- jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+	<script type="text/javascript">
+		var display = [];
+		var pictures = <?= json_encode($picturesLoad) ?>;
+
+		$(window).on("scroll", function() {
+			if(pictures.length > 0 && amountscrolled() > 70) {
+				display = pictures.slice(0, 12)
+				pictures = pictures.slice(13)
+				display.forEach(function(item){
+					$('#images').append('<img class="img-fluid col-sm col-lg-3 p-0" src="'+item+'" alt="">')
+				})
+			}
+		})
+
+		function amountscrolled(){
+			var winheight = $(window).height()
+			var docheight = $(document).height()
+			var scrollTop = $(window).scrollTop()
+			var trackLength = docheight - winheight
+			var pctScrolled = Math.floor(scrollTop/trackLength * 100)
+			return pctScrolled
+		}
+	</script>
 </body>
 </html>
